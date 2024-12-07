49f897afdeffe7743ecb17dc454faa223b6914dd
tweezerchef <ltomblock@gmail.com>
2024-12-07 16:29:26 -0600

    basic grid setup

---
+++ app/components/Services.tsx
@@ -0,0 +1,13 @@
+import styles from "./css/Services.module.css";
+export const Services = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box1}>1</div>
+      <div className={styles.box2}>2</div>
+      <div className={styles.box3}>3</div>
+      <div className={styles.box4}>4</div>
+      <div className={styles.box5}>5</div>
+      <div className={styles.box6}>6</div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./css/page.module.css";
 import { About } from "./components/About";
 import AboutDivider from "./components/AboutDivider";
-
+import { Services } from "./components/Services";
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -9,6 +9,8 @@ export default function Home() {
       <main className={styles.main}>
         <About />
         <AboutDivider />
+        <h2>Services</h2>
+        <Services />
       </main>
     </div>
   );
